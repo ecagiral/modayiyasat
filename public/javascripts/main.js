41a88bbd0ffeb4c72952afec9be23502a43f9cc1
wholalotta <ardaaskin@gmail.com>
2013-10-01 18:13:38 +0300

    commit video bg

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,20 @@
-
 $(document).ready(function(){
 
-
-	
+$(".next-icon").mouseover(function(e) {
+	e.preventDefault();
+	$('#header').animate({
+		"opacity":"1"
+	});
 });
 
+// Next button click goes to next div
+$('#linksContainer > DIV').on('click', function() {
+	
+	$('#linksContainer > DIV').removeClass("activeHeaderLink");
+	$(this).addClass("activeHeaderLink");
+	$('#page').animate({
+		"opacity":"1"
+	});    
+});
+
+});
