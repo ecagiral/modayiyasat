cf9e5301a88ca1e6f8204ade45b6104327029ea6
wholalotta <ardaaskin@gmail.com>
2013-10-01 18:22:17 +0300

    .

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,12 +9,25 @@ $(".next-icon").mouseover(function(e) {
 
 // Next button click goes to next div
 $('#linksContainer > DIV').on('click', function() {
-	
 	$('#linksContainer > DIV').removeClass("activeHeaderLink");
-	$(this).addClass("activeHeaderLink");
-	$('#page').animate({
-		"opacity":"1"
-	});    
+	if( $(this).is("#anasayfa") ) {
+		$(this).addClass("activeHeaderLink");
+		$('#page').animate({
+			"opacity":"0"
+		});
+		$("#bottomBlackBorder").animate({
+			"opacity":"0"
+		});
+	}else {		
+		$(this).addClass("activeHeaderLink");
+		$('#page').animate({
+			"opacity":"1"
+		});   
+		$("#bottomBlackBorder").animate({
+			"opacity":"1"
+		});
+	}
+	 
 });
 
 });
