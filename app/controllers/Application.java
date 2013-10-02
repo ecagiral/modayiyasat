f84d10aa9f76de07cde4d40fa89748d01f53375e
wholalotta <ardaaskin@gmail.com>
2013-10-02 15:56:07 +0300

    send feedback form

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Email;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -9,6 +11,24 @@ public class Application extends Controller {
 
     public static void index() {
         render();
+    }
+    
+    public static void sendMessage(@Required String firstname, @Required String lastname, @Required String email, String phone, @Required String message){
+    	
+        if (validation.hasErrors()) {
+            validation.keep();
+            params.flash();
+            flash.error("Bazı alanları boş bıraktın?");
+            index();            
+        }   	
+    	try {
+			Mails.sendFeedbackMail(firstname, lastname, email, phone, message);
+			flash.success("Mesajın gönderildi");
+			index();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}    	
+    	
     }
 
 }
