3a608b625381a78150ddb31e3833a169a1a7fedd
wholalotta <ardaaskin@gmail.com>
2013-10-04 12:52:34 +0300

    galeri etc

--- app/controllers/Mails.java
+++ app/controllers/Mails.java
@@ -9,10 +9,9 @@ import javax.mail.internet.*;
 public class Mails extends Mailer {
     
     public static void sendFeedbackMail(String firstname, String lastname, String email, String phone,String feedback) throws Exception {
-        setFrom(new InternetAddress("info@yolyola.com", "modayiyasat"));        
+        setFrom(new InternetAddress("info@modayiyasat.com", "modayiyasat"));        
         setSubject("Yeni mesaj var");
-        addRecipient("nerds@hep.com.tr");
-        //pr.turkiye@hm.com
+        addRecipient("pr.turkiye@hm.com");
         send(firstname,lastname,feedback,email,phone);
     }
     
