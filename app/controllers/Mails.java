41cc3aeb65cc12dcd03d6ba28cf2309eeec8ac28
wholalotta <ardaaskin@gmail.com>
2013-10-07 15:42:49 +0300

    mail adres updated

--- app/controllers/Mails.java
+++ app/controllers/Mails.java
@@ -11,7 +11,7 @@ public class Mails extends Mailer {
     public static void sendFeedbackMail(String firstname, String lastname, String email, String phone,String feedback) throws Exception {
         setFrom(new InternetAddress("info@modayiyasat.com", "modayiyasat"));        
         setSubject("Yeni mesaj var");
-        addRecipient("pr.turkiye@hm.com");
+        addRecipient("pr.turkey@hm.com");
         send(firstname,lastname,feedback,email,phone);
     }
     
